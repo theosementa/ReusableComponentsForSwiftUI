2ba545b9950cd58c23aebf6460c4bef243dd202a
Theo Sementa <theosementa@gmail.com>
2024-03-15 18:48:28 +0100

    Initial Commit

---
+++ ReusableComponentsForSwiftUI/ReusableComponentsForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableComponentsForSwiftUIApp.swift
+//  ReusableComponentsForSwiftUI
+//
+//  Created by KaayZenn on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReusableComponentsForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
